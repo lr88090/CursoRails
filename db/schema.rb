67aea7caf004f0576fbe64715b9224ceefa67592
lr88090 <lr88090@outlook.com>
2018-10-25 21:13:14 -0500

    Se crea la base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_26_005125) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "description"
+    t.integer "likes_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
